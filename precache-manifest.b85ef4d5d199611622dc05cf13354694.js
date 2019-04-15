865eeaf201cb02ab402fcc334891b3865f69b076
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-15 16:33:38 +0200

    Updates

---
+++ precache-manifest.b85ef4d5d199611622dc05cf13354694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96824de90d06e8e220f",
+    "url": "/redux-demo/static/css/main.b5c75f30.chunk.css"
+  },
+  {
+    "revision": "c96824de90d06e8e220f",
+    "url": "/redux-demo/static/js/main.62a87d26.chunk.js"
+  },
+  {
+    "revision": "00c31ebe46f0c4017d42",
+    "url": "/redux-demo/static/js/runtime~main.05d3d0d4.js"
+  },
+  {
+    "revision": "c5fd39dec34bc81688c8",
+    "url": "/redux-demo/static/js/2.a053c8f8.chunk.js"
+  },
+  {
+    "revision": "df800f0f65ab7f246e052593b3d46bca",
+    "url": "/redux-demo/index.html"
+  }
+];
