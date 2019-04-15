096fbfb838309cd4de73cd53704e02e7011fb373
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-15 16:16:29 +0200

    Change build folder

---
+++ docs/precache-manifest.508ffc5690af154a80e6e5cd26399f7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb250689719ba3d1ea50",
+    "url": "/static/css/main.b5c75f30.chunk.css"
+  },
+  {
+    "revision": "eb250689719ba3d1ea50",
+    "url": "/static/js/main.71c6892f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5fd39dec34bc81688c8",
+    "url": "/static/js/2.a053c8f8.chunk.js"
+  },
+  {
+    "revision": "d907d3d7c3eedb3aaf81124490d19393",
+    "url": "/index.html"
+  }
+];
